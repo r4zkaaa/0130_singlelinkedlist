7e383a60598dc16746e6b0b7409e36e30bdcb8b3
r4zkaaa <rzkiiahmd11@gmail.com>
2026-04-23 11:22:40 +0700

    Menambahkan node baru 2

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -35,3 +35,9 @@ public:
             cout << "\nDuplikasi noMhs tidak diijinkan\n";
             return;
         }
+
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
+    }
