af6f64fcc82a397f043c04d2e404f077d3df4c19
r4zkaaa <rzkiiahmd11@gmail.com>
2026-04-23 11:44:11 +0700

    Menambahkan void traverse

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -42,13 +42,6 @@ public:
     }
     }
 
-    class linkedList
-    {
-        void addNode();
-          if (START == NULL || nim <= START->noMHS
-            return;
-        )
-
         Node *previous = START;
         Node *current = START;
 
@@ -84,4 +77,28 @@ public:
         }
         return current != NULL;
     }
-    
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+        
+        if(current == START)
+           START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+    }
